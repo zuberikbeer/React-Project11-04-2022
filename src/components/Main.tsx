acd3a50d61590323df2e83582a7c44c74b0d67b4
Brandon <1bddouglas@gmail.com>
2022-11-07 13:58:25 -0500

    update 2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+import ComplexSearchResponse from "../models/ComplexSearchResponse";
 import { getRecipe } from "../services/RecipeApiServices";
 import "./Main.css";
+import Recipe from "../models/Recipe";
 
 const Main = () => {
-  getRecipe().then((res) => {
-    console.log(res.results);
-  });
-  return <div className="Main">Main works</div>;
+  const [recipeResults, setRecipeResults] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    getRecipe().then((res) => {
+      setRecipeResults(res.results);
+      console.log(res.results);
+    });
+  }, []);
+
+  return (
+    <div className="Main">
+      <ul>
+        {recipeResults.map((result) => (
+          <li key={result.id} title={result.title}>
+            <p>{result.title}</p>
+            <img src={result.image} alt={result.title} />
+          </li>
+        ))}
+        ;
+      </ul>
+    </div>
+  );
 };
 
 export default Main;
--- src/services/RecipeApiServices.ts
+++ src/services/RecipeApiServices.ts
@@ -9,3 +9,14 @@ export const getRecipe = (): Promise<ComplexSearchResponse> => {
     .then((res) => res.data)
     .catch((err) => console.log(err));
 };
+
+export const getRecipeInformation = (
+  id: string
+): Promise<ComplexSearchResponse> => {
+  return axios
+    .get(`https://api.spoonacular.com/recipes/${id}/information`, {
+      params: { apiKey: apiKey },
+    })
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+};
